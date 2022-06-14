2b53ab591517db9d02406fc1d8e6f9b9b8794e44
hakkicansengonul <coder.9734@gmail.com>
2022-06-14 15:40:07 +0300

    Initial Commit

---
+++ H4XOR-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR-SwiftUI
+//
+//  Created by hakkı can şengönül on 14.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4XOR-SwiftUI/H4XOR_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_SwiftUIApp.swift
+//  H4XOR-SwiftUI
+//
+//  Created by hakkı can şengönül on 14.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
